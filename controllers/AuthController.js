cec74e7778a370b76ed15406194e6da48c7936c6
Subhasis Das <subhasisdas125@gmail.com>
2020-05-08 02:22:28 +0530

    added auth middleware

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,5 @@
 const { User, validateUser } = require("../models/user.model");
+const bcrypt = require("bcryptjs");
 
 exports.signup = async (req, res) => {
   try {
@@ -29,15 +30,20 @@ exports.login = async (req, res) => {
       if (error) {
         return res.status(400).json({ error: error.details[0].message });
       } else {
+        //check if user exists in DB
         let user = await User.findOne({ username: req.body.username });
+        //return error message if not
         if (!user) {
           return res.status(404).json({ error: "No user found!" });
         } else {
+          //compare passwords on finding user
           let valid = await bcrypt.compare(req.body.password, user.password);
           if (valid) {
+            //return JWT on successful match
             let token = user.generateAuthToken();
             return res.status(200).json({ token });
           } else {
+            //send error message if passwords don't match
             return res
               .status(401)
               .json({ error: "Invalid username or password!" });
---
+++ middlewares/isAuthenticated.js
@@ -0,0 +1,18 @@
+//middleware to protect routes
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  let token = req.header("x-auth-token");
+  if (!token) {
+    return res.status(403).json({ error: "Access denied, no token provided!" });
+  } else {
+    try {
+      const payload = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
+      req.user = payload;
+      next();
+    } catch (error) {
+      console.error(error);
+      return res.status(400).json({ error });
+    }
+  }
+};
