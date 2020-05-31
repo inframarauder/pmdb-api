c73e0e74f8c1dcefcfdb75b107056a38c8cb1523
Subhasis Das <subhasisdas125@gmail.com>
2020-05-31 15:40:16 +0530

    user update route

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,12 +1,35 @@
 const User = require("../models/user.model");
 
-exports.readUser = async (req, res) => {
+exports.read = async (req, res) => {
   try {
     let user = await User.findById(req.params.id, { password: 0, type: 0 });
     if (!user) {
       res.status(404).json({ error: "User not found!" });
     } else {
       res.status(200).json(user);
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal Server Error!" });
+  }
+};
+
+exports.update = async (req, res) => {
+  try {
+    let { username, email } = req.body;
+    let user = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        username: username,
+        email: email,
+      },
+      { new: true, runValidators: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found!" });
+    } else {
+      return res.status(200).json(user);
     }
   } catch (error) {
     console.error(error);
