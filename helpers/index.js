4d52f7f517c90746488e28f50221eb335c0c0b09
subhasis020299 <subhasisdas125@gmail.com>
2020-07-25 00:21:03 +0530

    some fix that i do not fully understand!

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,6 @@
 const Movie = require("../models/movie.model");
 const Review = require("../models/review.model");
+const { number } = require("joi");
 
 exports.updateRating = async (review) => {
   try {
@@ -8,9 +9,18 @@ exports.updateRating = async (review) => {
       { rating: 1, _id: 0 }
     ).lean();
 
-    let avgRating =
-      reviews.reduce((acc, currVal) => acc.rating + currVal.rating).rating /
-      reviews.length;
+    let sumOfRatings = reviews.reduce(
+      (acc, currVal) => acc.rating + currVal.rating
+    );
+
+    //some fix that i dont understand!
+    if (typeof sumOfRatings !== number) {
+      sumOfRatings = reviews.reduce(
+        (acc, currVal) => acc.rating + currVal.rating
+      ).rating;
+    }
+
+    let avgRating = sumOfRatings / reviews.length;
 
     await Movie.findByIdAndUpdate(
       review.movie,
